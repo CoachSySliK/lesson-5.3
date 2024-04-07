d4f8c1e85262121763f7bee707be31ac58e80ce8
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-07 18:13:35 +0300

    Добавьте файлы проекта.

---
+++ lesson 5.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    
+    cout << "Введите год: \n";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                cout << "Високосный";
+                return 0;
+            }
+            cout << "Не високосный";
+            return 0;
+        }
+        cout << "Високосный";
+        return 0;
+    }
+    else {
+        cout << "Не високосный";
+    }
+    return 0;
+}
